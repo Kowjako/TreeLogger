0ee97dae349263c2da68611a4c501777152908bf
Włodzimierz Kowjako <kowyako@yandex.ru>
2022-03-25 17:11:49 +0100

    Add interface to implement

---
+++ TreeLogger/Interfaces/ILoggerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TreeLogger.ViewModel;
+
+namespace TreeLogger.Interfaces
+{
+    public interface ILoggerViewModel
+    {
+        CancellationTokenSource CancelTokenSource { get; set; }
+        event Action<TreeNode> OnNodeAdded;
+        event Action RefreshTree;
+        event Action RefreshLoggerTime;
+        ICollection<TreeNode> RootNodes { get; }
+        TreeNode MainRootNode { get; set; }
+        void CreateMainRootNode(string caption, MessageSeverity e);
+        void CreateChildNode(string caption, MessageSeverity e);
+        void CreateChildNode(string caption, MessageSeverity e, TreeNode parentNode);
+        void ShowExample();
+        void HandleCancellationToken();
+
+    }
+}
