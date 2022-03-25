ff065b8b23cf82c353b183d3e3ab81495998204c
Włodzimierz Kowjako <kowyako@yandex.ru>
2022-03-25 17:13:05 +0100

    Start implementation logger logic

---
+++ TreeLogger/LoggerExcpetion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeLogger
+{
+    class LoggerExcpetion : Exception
+    {
+        public LoggerExcpetion(string message) : base(message)
+        {
+
+        }
+    }
+}
