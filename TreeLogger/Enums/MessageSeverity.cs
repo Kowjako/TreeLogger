b92a5a9d3a5bac54206981c77b250b755de712f6
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-06-13 17:40:33 +0200

    Add default logger runner and message severity enum

---
+++ TreeLogger/Enums/MessageSeverity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeLogger.Enums
+{
+    public enum MessageSeverity
+    {
+        None = 0,
+        Information = 1,
+        Error = 2,
+        Warning = 3,
+        Exception = 4
+    }
+}
---
+++ TreeLogger/Global/TreeLoggerRunner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TreeLogger.Global
+{
+    public static class TreeLoggerRunner
+    {
+        /// <summary>
+        /// Standard method to invoke logger execution
+        /// </summary>
+        /// <param name="caption">Logger start operation caption</param>
+        /// <param name="action">Method which should be logged</param>
+        public static void RunTreeLogger(string caption, Action<ILogger> action)
+        {
+
+        }
+
+        /// <summary>
+        /// Standard method to invoke logger execution supports async cancellation
+        /// </summary>
+        /// <param name="caption">Logger start operation caption</param>
+        /// <param name="action">Method which should be logged</param>
+        public static void RunTreeLogger(string caption, Action<ILogger, CancellationToken> action)
+        {
+
+        }
+    }
+}
