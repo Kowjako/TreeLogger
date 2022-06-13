91836206873e3f9ba79310fdd88cae818b1da07b
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-06-13 17:40:46 +0200

    Add start interfaces

---
+++ TreeLogger/Interfaces/ILoggerView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeLogger.Interfaces
+{
+    /// <summary>
+    /// Interface which describe TreeLogger view implementations
+    /// </summary>
+    public interface ILoggerView : IDisposable
+    {
+        /// <summary>
+        /// Used to attach view model to view of logger
+        /// </summary>
+        /// <param name="viewModel"></param>
+        void SetViewModel(ILoggerViewModel viewModel);
+
+        /// <summary>
+        /// Property to get execution time
+        /// </summary>
+        TimeSpan ElapsedTime { get; }
+
+        /// <summary>
+        /// Method to show logger window
+        /// </summary>
+        /// <returns></returns>
+        DialogResult ShowDialog();
+
+        /// <summary>
+        /// Token which used to async logger cancellation
+        /// </summary>
+        CancellationToken CancellationToken { get; }
+    }
+}
--- TreeLogger/Properties/AssemblyInfo.cs
+++ TreeLogger/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TreeLogger")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Asynchronous TreeLogger, including async cancellation")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TreeLogger")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright Wlodzimierz Kowjako ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
