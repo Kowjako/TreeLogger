3c72c6806d31a38fa88fe3c5ecfbe1757274fef2
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-06-14 17:09:29 +0200

    Added test application for logger

---
+++ TreeLoggerTestApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TreeLogger.Global;
+
+namespace TreeLoggerTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TreeLoggerRunner.RunTreeLogger("Ksiegowanie dokumentu handlowego", (logger, token) =>
+            {
+                for(int i = 0;i <10;i++)
+                {
+                    if (token.IsCancellationRequested) return;
+                    logger.LogMessage($"Aktualna wartosc : {i}", TreeLogger.Enums.MessageSeverity.Information);
+                    Thread.Sleep(500);
+                }
+            });
+        }
+    }
+}
